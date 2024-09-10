1b04e5a760d83c45c463cb6fcb0631b9841bb9b0
Jeeraphat111 <oppoa5syouii@gmail.com>
2024-09-10 20:48:19 +0700

    Create README.cpp

---
+++ README.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int goge,num,sum,ber;
+
+    while(true){
+        cout<<"Enter Num :";
+        cin>>num;
+
+        if(num%2==0){
+            cout<<"เลขคู่";
+        }else{
+            cout<<"เลขคี่";
+        }
+
+    }
+
+    return 0 ;
+}
